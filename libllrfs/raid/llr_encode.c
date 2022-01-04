944c8bd2f1aa9e3afd0d556f500ecea94f32faeb
raid5atemyhomework <raid5atemyhomework@protonmail.com>
2022-01-04 13:34:26 +0800

    libllrfs: Implement encoding of parity blocks.

--- libllrfs/raid/llr_encode.c
+++ libllrfs/raid/llr_encode.c
@@ -24,3 +24,71 @@ SOFTWARE.
 #endif
 #include"llr_cauchy.h"
 #include"llr_encode.h"
+#include"llr_util.h"
+#include"llr_xorgf.h"
+
+void llr_encode(void const* const* data_blocks,
+		unsigned int num_data_blocks,
+		void* const* parity_blocks,
+		unsigned int num_parity_blocks) {
+	unsigned int i, j;
+
+	if (num_parity_blocks == 0)
+		/* Nothing to do...?  */
+		return;
+
+	if (num_data_blocks == 0) {
+		/* No parity...  */
+		for (j = 0; j < num_parity_blocks; ++j) {
+			llr_memzero(parity_blocks[j], LLR_XORGF_BLOCK_SIZE);
+		}
+		return;
+	}
+
+	/* Initialize the parity blocks from the first data block.  */
+	/* assert(llr_cauchy(0, j) == 1); */
+	if (!data_blocks[0]) {
+		for (j = 0; j < num_parity_blocks; ++j) {
+			llr_memzero(parity_blocks[j], LLR_XORGF_BLOCK_SIZE);
+		}
+	} else {
+		for (j = 0; j < num_parity_blocks; ++j) {
+			llr_memcpy(parity_blocks[j], data_blocks[0], LLR_XORGF_BLOCK_SIZE);
+		}
+	}
+
+	for (i = 1; i < num_data_blocks; ++i) {
+		/* Skip data blocks that are all-0s/nonexistent.  */
+		if (!data_blocks[i])
+			continue;
+
+		/* Parity block 0 is just RAID5.  */
+		/* assert(llr_cauchy(i, 0) == 1); */
+		llr_xorgf_acc_mul(parity_blocks[0], 1, data_blocks[i]);
+
+		for (j = 1; j < num_parity_blocks; ++j) {
+			unsigned char factor = llr_cauchy(i, j);
+			llr_xorgf_acc_mul(parity_blocks[j], factor, data_blocks[i]);
+		}
+	}
+}
+
+void llr_encode_modify(void const* restrict delta_data_block,
+		       unsigned int data_idx,
+		       void* const* parity_blocks,
+		       unsigned int num_parity_blocks) {
+	unsigned int i = data_idx;
+	unsigned int j;
+
+	if (num_parity_blocks == 0)
+		/* Nothing to do...?  */
+		return;
+
+	/* Parity block 0 is just RAID5.  */
+	llr_xorgf_acc_mul(parity_blocks[0], 1, delta_data_block);
+
+	for (j = 1; j < num_parity_blocks; ++j) {
+		unsigned char factor = llr_cauchy(i, j);
+		llr_xorgf_acc_mul(parity_blocks[j], factor, delta_data_block);
+	}
+}
--- libllrfs/raid/llr_encode.h
+++ libllrfs/raid/llr_encode.h
@@ -23,4 +23,64 @@ SOFTWARE.
 #if !defined(RAID_LLR_ENCODE_H_)
 #define RAID_LLR_ENCODE_H_
 
+/*
+This module performs encoding, i.e. computing the
+parity blocks from the data blocks.
+It also provides a function to perform modification,
+i.e. given the difference between two data blocks,
+modify the parity block.
+*/
+
+/** llr_encode
+ *
+ * @brief Computes all parity blocks from the given
+ * data blocks.
+ *
+ * @param data_blocks - An array of pointers to the
+ * data blocks.
+ * Data blocks are of LLR_XORGF_BLOCK_SIZE bytes.
+ * An entry in this array may be NULL, indicating
+ * that we should treat the data block as being all
+ * 0.
+ * @param num_data_blocks - The number of data blocks.
+ * @param parity_blocks - An array of pointers to the
+ * parity blocks.
+ * Parity blocks are of LLR_XORGF_BLOCK_SIZE bytes.
+ * All entries must be non-NULL.
+ * Parity blocks must have unique locations in memory,
+ * i.e. they cannot share storage with each other or
+ * with any data blocks.
+ * @param num_parity_blocks - The number of parity
+ * blocks.
+ */
+void llr_encode(void const* const* data_blocks,
+		unsigned int num_data_blocks,
+		void* const* parity_blocks,
+		unsigned int num_parity_blocks);
+
+/** llr_encode_modify
+ *
+ * @brief Modifies the parity blocks, given the delta
+ * between the old data block and the new data block.
+ *
+ * @param delta_data_block - A buffer containing the
+ * XOR between the old version of the data block and
+ * the new version of the data block.
+ * @param data_idx - The index of the data block being
+ * modified.
+ * @param parity_blocks - An array of pointers to the
+ * parity blocks.
+ * Parity blocks are of LLR_XORGF_BLOCK_SIZE bytes.
+ * All entries must be non-NULL.
+ * Parity blocks must have unique locations in memory,
+ * i.e. they cannot share storage with each other or
+ * with the delta data block.
+ * @param num_parity_blocks - The number of parity
+ * blocks.
+ */
+void llr_encode_modify(void const* restrict delta_data_block,
+		       unsigned int data_idx,
+		       void* const* parity_blocks,
+		       unsigned int num_parity_blocks);
+
 #endif /* !defined(RAID_LLR_ENCODE_H_) */
